f9ef8bc8d4cc393c5b2264500a44fa489727391f
Abdalazez-Sabry <abdalazezsabry00@gmail.com>
2025-05-20 13:00:06 +0300

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import '../app.css';
 	import Footer from './Footer.svelte';
 	import Navbar from './Navbar.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { dev } from '$app/environment';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 	onNavigate((navigation) => {
